822a6989a70282c80e7d9c584fd3e6d43f8f66eb
takahiro-saeki <hiroooon69@gmail.com>
2016-08-17 14:52:49 +0900

    commit!

--- assets/js/container/Main.js
+++ assets/js/container/Main.js
@@ -1,7 +1,29 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import Test from '../component/Test';
+import request from 'superagent';
 
-let Main = connect()(Test);
+function hogee() {
+  return console.log('test')
+}
+
+function fooo() {
+  request
+    .get('https://www.tunefind.com/api/v1/show')
+    .withCredentials()
+    .type('xml')
+    .end((err, res) => {
+      if(err) {
+        console.log(err)
+      } else {
+        console.log(res)
+      }
+    })
+}
+
+let Main = connect(
+  hogee,
+  fooo
+)(Test);
 
 export default Main;
